b3d11f8fee192ed7f5a2dfb3151c2e43be4faba4
mbalanya <okobacharles@yahoo.co.uk>
2020-06-15 11:45:10 +0300

    apply form validation

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -10,7 +10,7 @@ export class QuoteComponent implements OnInit {
 
   quotes:Quote[] = [
     new Quote(1, 'Person1', 'This is a sample quote', new Date(2019,6,9)),
-    new Quote(2, 'Person2', 'This is another sample quote', new Date(2022,1,12)),
+    new Quote(2, 'Person2', 'This is another sample quote', new Date(2020,1,12)),
     new Quote(3, 'Person3', 'This is a third sample quote', new Date(2019,0,18)),
     new Quote(4, 'Person4', 'This is a forth sample quote', new Date(2019,2,14))
   ];
